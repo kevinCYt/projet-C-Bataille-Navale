e35913b04d8abda9a64f99e975dff0d5b5151e83
kevinCYt <154614248+kevinCYt@users.noreply.github.com>
2023-12-24 18:16:37 +0100

    vie.c

--- vie.c
+++ vie.c
@@ -8,7 +8,7 @@
  */
  
 #include <stdio.h>
-#include "bateau.h"
+#include "constante.h"
 #include "case.h"
 #include "plateau.h"
 
@@ -16,39 +16,22 @@
 /**
  * @brief Check if a boat is still alive on the game board.
  *
- * This function determines whether a boat is still alive on the game board based on its position, size, and orientation.
- * If the boat is oriented vertically, it checks the corresponding column for any non-WRECK cells.
- * If the boat is oriented horizontally, it checks the corresponding row for any non-WRECK cells.
+ * This function determines whether there is still boat alive on the game board 
  *
- * @param bateau Pointer to the boat struct.
  * @param plateau Pointer to the game board struct.
- * @return 1 if the boat is still alive, 0 if the boat is wrecked.
+ * @return 1 if there is boat still alive, 0 if all boat are wrecked.
  */
  
  
- 
-int EnVie(struct Bateau *bateau, struct Plateau *plateau){
-	int colonne = bateau->colonne;
-	int ligne = bateau->ligne;
-	int taille = bateau->taille;
-	int orientation = bateau->orientation;
-	if(orientation==1){ 									//vertical
-		for(int i = ligne; i<ligne+taille; i++){ 	
-		
-        	
-			if(plateau->plato[i][colonne] != WRECK){ 
-				return 1;
-			}
-		}
-	}
-	if(orientation==0){ 									//horizontal
-		for(int i = colonne; i<colonne+taille; i++){	
-			if(plateau->plato[ligne][i] != WRECK){ 
-				return 1;		
-			}
-		
-		}
-	}
-	return 0;	//WRECKED BOAT
 
+int alive(struct Plateau *plateau){
+	int taille = COTE;
+	for (int i = 0; i < taille; i++) {
+        for (int j = 0; j < taille; j++) {
+            if (plateau->plato[i][j] == BOAT) {
+                return 1;
+		     }
+        }
+    }
+	return 0;	//no boat alive
 }
